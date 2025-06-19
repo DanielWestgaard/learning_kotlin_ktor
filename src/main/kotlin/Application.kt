0d573b30cedb5bcf08ce4964cc25c16f28f9ded9
DanielWestgaard <daniel.westgaard@gmail.com>
2025-06-19 23:33:42 +0200

    able to run now by installing authentication before routes

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,16 +1,20 @@
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-
 import plugins.*
 
-fun main()  {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module).start(wait = true)
+fun main() {
+    embeddedServer(
+        Netty,
+        port = 8080,
+        host = "0.0.0.0",
+        module = Application::module
+    ).start(wait = true)
 }
 
 fun Application.module() {
-    configureRouting()
-    configureSerialization()
-    configureSecurity()
-    configureCORS()
+    configureSerialization()  // Install JSON handling first
+    configureSecurity()       // Install authentication
+    configureCORS()          // Install CORS
+    configureRouting()       // Install routes last (they depend on auth)
 }
